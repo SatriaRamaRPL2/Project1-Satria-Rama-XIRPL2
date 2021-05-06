4fc632d8506d2971dd01c7ed66f2b1d7e69a3491
SatriaRamaRPL2 <80035214+SatriaRamaRPL2@users.noreply.github.com>
2021-05-06 12:41:53 +0700

    Delete aplikasirestauran directory

--- aplikasirestauran/Source Packages/classes/MainAplikasiKasir.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class MainAplikasiKasir {
-
-	public static void main (String[] args) {}
-	public void generateDaftarMenu () {}
-
-}
--- aplikasirestauran/Source Packages/classes/Ramen.java
+++
@@ -1,6 +0,0 @@
-package classes;
-
-public class Ramen extends Menu {
-	public Ramen (String nama_ramen, double harga) { }
-
-}
